064a4e7db71eea1f0229e0a872d14558fff6e315
NghtZer0 <1494085609@qq.com>
2023-04-14 22:18:56 +0800

    Site updated: 2023-04-14 22:18:55

--- js/f12.js
+++ js/f12.js
@@ -1,3 +1,3 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>document.onkeydown = function (e) {
+document.onkeydown = function (e) {
     if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("what are you xxxxxxxx doing？You are such a little son of a xxxxx "), event.keyCode = 0, event.returnValue = !1, !1
 };
